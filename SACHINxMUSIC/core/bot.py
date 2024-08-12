a1294b7cf91d903a29c4bcba15b295c04429fdff
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-08-12 11:58:28 +0530

    Update bot.py

--- SACHINxMUSIC/core/bot.py
+++ SACHINxMUSIC/core/bot.py
@@ -1,17 +1,16 @@
 from pyrogram import Client, errors
 from pyrogram.enums import ChatMemberStatus
-from SACHINxMUSIC.core.bot import RAUSHAN
 
 import config
 
 from ..logging import LOGGER
 
 
-class SACHINxMUSIC(Client):
+class RAUSHAN(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="SACHINxMUSIC",
+            name="RAUSHAN",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
