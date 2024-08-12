6179b90125825d319dfd056ce8b3584153ba2d16
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-08-12 12:09:17 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHINxMUSIC/plugins/admins/resume.py
+++ SACHINxMUSIC/plugins/admins/resume.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from SACHINxMUSIC import app
-from SACHINxMUSIC.core.call import SACHINxMUSIC
+from SACHINxMUSIC.core.call import RAUSHAN
 from SACHINxMUSIC.utils.database import is_music_playing, music_on
 from SACHINxMUSIC.utils.decorators import AdminRightsCheck
 from SACHINxMUSIC.utils.inline import close_markup
