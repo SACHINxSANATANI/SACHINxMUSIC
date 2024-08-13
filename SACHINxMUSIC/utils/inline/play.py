92ba1030290914d5c887321d46615550979eb796
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-08-13 10:50:48 +0530

    Update play.py

--- SACHINxMUSIC/utils/inline/play.py
+++ SACHINxMUSIC/utils/inline/play.py
@@ -1,8 +1,7 @@
 import math
+
 from config import SUPPORT_CHAT, OWNER_ID
-from pyrogram.types import InlineKeyboardButton
-from SACHINxMUSIC import app
-import config
+
 from SACHINxMUSIC.utils.formatters import time_to_seconds
 
 
