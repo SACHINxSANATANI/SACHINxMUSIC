68cf84ae8312be4b90a0dec5304e33ac904c1b82
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-08-13 06:44:32 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHINxMUSIC/plugins/tools/welcome.py
+++ SACHINxMUSIC/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from SACHINxMUSIC.utils.SACHINxMUSIC_ban import admin_filter
+from SACHINxMUSIC.utils.SACHIN_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
