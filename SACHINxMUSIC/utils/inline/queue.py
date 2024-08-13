bfc1705d6eb82dbf7d19af145a0dfdb8f1d80b6f
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-08-13 16:25:03 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- SACHINxMUSIC/utils/inline/queue.py
+++ SACHINxMUSIC/utils/inline/queue.py
@@ -66,10 +66,18 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
+         InlineKeyboardButton(text="• ʌᴅᴅ ϻɛ ʙʌʙʏ •️", url=f"https://t.me/HIMANSHI_MUSIC_BOT?startgroup=true",)
+        ],
+        [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+         InlineKeyboardButton(text="• sꭎ֟፝᷍ᴘᴘσʀᴛ •", url=f"https://t.me/All_sanatani_bot",),
+         InlineKeyboardButton(text="• ꭎ֟፝᷍ᴘᴅʌᴛɛ •", url=f"https://t.me/+Ckzm2ypQyIIzZTll",),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
